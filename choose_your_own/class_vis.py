86f0599392aa581b4a34ddb093d9f398d053e86b
Lynnhom Xue <xlh1221it@gmail.com>
2016-02-18 17:18:08 +0800

    start adventure

--- choose_your_own/class_vis.py
+++ choose_your_own/class_vis.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pylab as pl
 
-def prettyPicture(clf, X_test, y_test):
+def prettyPicture(clf, X_test, y_test, workdir="C:/Users/dm1-3266/Desktop/"):
     x_min = 0.0; x_max = 1.0
     y_min = 0.0; y_max = 1.0
     
@@ -33,7 +33,7 @@ def prettyPicture(clf, X_test, y_test):
     plt.xlabel("bumpiness")
     plt.ylabel("grade")
 
-    plt.savefig("test.png")
+    plt.savefig(workdir + "test.png")
 
 import base64
 import json
