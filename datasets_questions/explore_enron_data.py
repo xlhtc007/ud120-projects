f796efa373c35e03b4ccc0d8af244096ee1f3112
Lynnhom Xue <xlh1221it@gmail.com>
2016-02-29 16:52:07 +0800

    start datasets questions

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -16,7 +16,23 @@
 """
 
 import pickle
+workspace_dir = "C:/Users/dm1-3266/PycharmProjects/ud120-projects/"
+enron_data = pickle.load(open(workspace_dir + "final_project/final_project_dataset.pkl", "r"))
 
-enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
+# size of the Enron Datasets
+print len(enron_data)
 
+# Features in the Enron Dataset
+print len(enron_data[enron_data.keys()[1]])
+
+# POIs in the Enron Data
+counter = 0
+for k in enron_data.keys():
+    if enron_data[k]["poi"]:
+        counter += 1
+
+print counter
+
+# Query the Dataset 1
+print enron_data["PRENTICE JAMES"]["total_stock_value"]
 
