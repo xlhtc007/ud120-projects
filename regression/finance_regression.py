fe67c390820ad98c7485109479e34c21c8f23c68
Lynnhom Xue <xlh1221it@gmail.com>
2016-03-08 17:04:51 +0800

    start regression

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -29,7 +29,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -37,9 +37,16 @@ test_color = "b"
 ### Please name it reg, so that the plotting code below picks it up and 
 ### plots it correctly. Don't forget to change the test_color above from "b" to
 ### "r" to differentiate training points from test points.
+from sklearn.linear_model import LinearRegression
+reg = LinearRegression()
+reg.fit(feature_train, target_train)
 
-
-
+print "slope:", reg.coef_
+print "intercept:", reg.intercept_
+print "\n########### stats on test dataset ###########\n"
+print "r-squared score:", reg.score(feature_test, target_test)
+print "\n########### stats on training dataset ###########\n"
+print "r-squared score:", reg.score(feature_train, target_train)
 
 
 
