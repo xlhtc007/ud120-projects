4662b0572b40e27d51c0974c08a839779d14c669
Lynnhom Xue <xlh1221it@gmail.com>
2016-03-14 17:10:49 +0800

    outliers

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,16 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
     cleaned_data = []
 
     ### your code goes here
+    for i in range(90):
+        cleaned_data.insert(i, (ages[i], net_worths[i], (predictions[i]-net_worths[i])**2))
 
-    
+    cleaned_data = sorted(cleaned_data, key=lambda t: t[2])
+    cleaned_data = cleaned_data[0:81]
+    print len(cleaned_data)
+
     return cleaned_data
 
